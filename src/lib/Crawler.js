ee0a3594383c51afcc331cce71c02011b8013ab4
fabrizio morroia <bicccio@gmail.com>
2018-12-03 07:59:35 +0100

    CHeck Black list before inserting in db and fix some bug

--- src/lib/Crawler.js
+++ src/lib/Crawler.js
@@ -3,12 +3,7 @@
 import request from "request-promise";
 import parserUrl from "url";
 import robots from "robots";
-import {
-  MAX_PAGES_TO_VISIT,
-  HEADERS,
-  REQUEST_TIMEOUT,
-  BLACKLIST
-} from "../../conf/config.json";
+import { MAX_PAGES_TO_VISIT, HEADERS, REQUEST_TIMEOUT, BLACKLIST } from "../../conf/config.json";
 import logger from "./log";
 
 export default class Crawler {
@@ -47,10 +42,10 @@ export default class Crawler {
 
         const { protocol, hostname } = parserUrl.parse(cleanUrl);
 
-        // const isVisited = await this.db.find({ url: cleanUrl, visited: true });
-        // if (isVisited && isVisited.length > 0) {
-        //   continue;
-        // }
+        const isVisited = await this.db.find({ url: cleanUrl, visited: true });
+        if (isVisited && isVisited.length > 0) {
+          continue;
+        }
 
         if (BLACKLIST.indexOf(hostname) > -1) {
           continue;
@@ -67,6 +62,9 @@ export default class Crawler {
         await this.visitPage(cleanUrl);
       } catch (error) {
         logger.error(this.currentUrl.url);
+
+        await this.db.update({ url: this.currentUrl.url }, { url: this.currentUrl.url, title: "", visited: true }, {});
+
         logger.error(error);
       }
     }
@@ -87,11 +85,7 @@ export default class Crawler {
 
       logger.info(`#${this.numPagesVisited}: ${url} - ${title}`);
 
-      await this.db.update(
-        { url: url },
-        { url: url, title: title, visited: true },
-        {}
-      );
+      await this.db.update({ url: url }, { url: url, title: title, visited: true }, {});
 
       if (links.length === 0) return;
 
@@ -104,9 +98,7 @@ export default class Crawler {
           completeUrl = cleanUrl;
         } else {
           const noLeadingSlashUrl = cleanUrl.replace(/^\/+/g, "");
-          completeUrl = `${this.protocol}//${
-            this.baseUrl
-          }/${noLeadingSlashUrl}`;
+          completeUrl = `${this.protocol}//${this.baseUrl}/${noLeadingSlashUrl}`;
         }
 
         //this.store.push(completeUrl);
@@ -118,7 +110,9 @@ export default class Crawler {
         if (res && res.length === 0) {
           // check for blacklist and allowed domains
           const { hostname } = parserUrl.parse(completeUrl);
-          if(this.isBlackListed(hostname)) continue
+
+          if (this.isBlackListed(hostname)) continue;
+
           await this.db.insert({
             url: completeUrl,
             title: "",
@@ -134,17 +128,13 @@ export default class Crawler {
   canFetch(url, userAgent) {
     const { hostname, protocol } = parserUrl.parse(url);
 
-    const parser = new robots.RobotsParser(
-      `${protocol}//"${hostname}/robots.txt`
-    );
+    const parser = new robots.RobotsParser(`${protocol}//${hostname}/robots.txt`);
 
     return parser.canFetchSync(userAgent, url);
   }
 
   isBlackListed(hostname) {
-    if (BLACKLIST.indexOf(hostname) > -1) {
-      continue;
-    }
+    return BLACKLIST.indexOf(hostname) > -1;
   }
 
   finish() {
